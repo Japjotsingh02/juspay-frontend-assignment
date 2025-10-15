36db5206f72406015d3e257b0b633c312af443e5
Japjotsingh02 <sjapjot2002@gmail.com>
2025-10-15 14:49:20 +0530

    fixed: rerouting issue

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,11 +1,13 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import DefaultDashboard from "./DefaultDashboard";
 
 const Dashboard = () => {
   return (
     <>
       <Routes>
+        <Route path="/" element={<Navigate to="/dashboards/default" replace />} />
         <Route path="/dashboards/default" element={<DefaultDashboard />} />
+        <Route path="*" element={<Navigate to="/dashboards/default" replace />} />
       </Routes>
     </>
   );
